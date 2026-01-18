0baf85389f14674d02c46d97f5494b94cc14a088
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2026-01-18 17:14:17 +0300

    feat: add proxy chain add new components

---
+++ lib/proxy-chain/types.ts
@@ -0,0 +1,8 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+export type ChainableProxy = (
+  request: NextRequest,
+  event: NextFetchEvent,
+) => Promise<NextResponse>;
+
+export type ProxyFactory = (proxy: ChainableProxy) => ChainableProxy;
