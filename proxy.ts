72d26c9784a38de97de7d43b4cbea6d5f0bb210f
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2026-01-18 17:17:31 +0300

    feat: implement proxy chain

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,15 @@
-import { updateSession } from "@/lib/supabase/proxy";
-import { type NextRequest } from "next/server";
+import { chain } from "@/lib/proxy-chain";
+import { withSupabaseSession } from "@/lib/supabase/proxy";
+import { type NextFetchEvent, type NextRequest } from "next/server";
 
-export async function proxy(request: NextRequest) {
-  return await updateSession(request);
+const proxies = [withSupabaseSession];
+
+export async function proxy(request: NextRequest, event: NextFetchEvent) {
+  return chain(proxies)(request, event);
 }
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
-     */
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
